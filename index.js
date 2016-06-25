b85f5f73af7e8a7025389ba831eea26d1d29b8be
unnamed42 <a390517426@gmail.com>
2016-06-25 18:53:41 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+hexo.extend.tag.register('spoiler', function(args) {
+    
+    var content = args.join(' ');
+    
+    var html_code = hexo.render.renderSync({text: content, engine: "markdown"});
+    
+    html_code = html_code.replace(/<(|\/)p>/g,'');
+    
+    var spoiler_result = "<span class=\"spoiler\">" + html_code + "</span>";
+    
+    return spoiler_result;
+});
+
+var fs = require('hexo-fs');
+var path = require('path');
+
+var assetBase = path.resolve(__dirname, "./asset");
+
+hexo.extend.generator.register('css', function(locals) {
+    
+    var route = {
+        path: 'css/spoiler.css',
+        data: function() {return fs.createReadStream(path.resolve(assetBase,'spoiler.css'));}
+    }
+    
+    return route;
+});
+
+hexo.extend.filter.register('after_post_render',function(data){
+    
+    var link_css = "<link rel=\"stylesheet\" href=\"/css/spoiler.css\" type=\"text/css\">"
+    
+    data.content = link_css + data.content;
+    
+    return data;
+});
