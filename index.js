124e479cd1e31ec61f7abf152b8ee0d4d5e65008
unnamed42 <a390517426@gmail.com>
2016-09-09 23:39:16 +0800

    Improved script

--- index.js
+++ index.js
@@ -12,35 +12,24 @@ hexo.extend.tag.register('spoiler', function(args) {
 var fs = require('hexo-fs');
 var path = require('path');
 
-var assetBase = path.resolve(__dirname, "./asset");
-
-hexo.extend.generator.register('css', function(locals) {
-    
-    var route = {
-        path: 'css/spoiler.css',
-        data: function() {return fs.createReadStream(path.resolve(assetBase,'spoiler.css'));}
-    }
-    
-    return route;
-});
-
-hexo.extend.generator.register('js', function(locals) {
-    
-    var route = {
-        path: 'js/spoiler.js',
-        data: function() {return fs.createReadStream(path.resolve(assetBase,'spoiler.js'));}
-    }
-    
-    return route;
+hexo.extend.generator.register('spoiler_asset', function(locals) {
+    var assetBase = path.resolve(__dirname, "./asset");
+    return [
+        {
+            path: 'css/spoiler.css',
+            data: function() {return fs.createReadStream(path.resolve(assetBase,'spoiler.css'));}
+        },
+        {
+            path: 'js/spoiler.js',
+            data: function() {return fs.createReadStream(path.resolve(assetBase,'spoiler.js'));}
+        }
+    ];
 });
 
 hexo.extend.filter.register('after_post_render',function(data){
-    
     var link_css = "<link rel=\"stylesheet\" href=\"/css/spoiler.css\" type=\"text/css\">";
     
     var link_js = "<script src=\"/js/spoiler.js\" type=\"text/javascript\"></script>";
     
-    data.content += link_css + '\n' +link_js;
-
-    return data;
+    data += link_css + link_js;
 });
