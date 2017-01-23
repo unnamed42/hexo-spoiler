859c902d33b74ec56152a97c1d840c12868e6eae
unnamed42 <a390517426@gmail.com>
2017-01-23 20:07:34 +0800

    Use DOM toggle

--- asset/spoiler.js
+++ asset/spoiler.js
@@ -1,10 +1,8 @@
 (function(){
     var spoiler = document.getElementsByClassName("spoiler");
-    var handler = function(){
-        var cname = this.className.trim();
-        cname === "spoiler"? cname += " revealed": cname = "spoiler";
-        this.className = cname;
-    };
-    for(var i = 0; i < spoiler.length; ++i)
-        spoiler[i].addEventListener("click",handler);
+    for(var i = 0; i < spoiler.length; ++i) {
+        spoiler[i].addEventListener("click", function() {
+            this.classList.toggle("revealed");
+        });
+    }
 })();
