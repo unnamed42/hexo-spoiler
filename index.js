d3d0629cd83ee4f98e297d08a6c14663814ec1c7
unnamed42 <a390517426@gmail.com>
2017-03-16 23:17:26 +0800

    fix for event not firing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ hexo.extend.tag.register('spoiler', function(args) {
     return "<span class=\"spoiler\">" + html_code + "</span>";
 });
 
-var css = "<style>" + 
+var css = '<style type="text/css">' + 
           "span.spoiler {" + 
               "color: rgba(0, 0, 0, 0);" + 
               "background-color: rgba(0, 0, 0, 0);" + 
@@ -28,14 +28,14 @@ var css = "<style>" +
           "</style>";
 
 var js = '<script type="text/javascript">' + 
-         "(function(){" + 
+         "window.addEventListener('load', function(){" + 
              "var spoiler = document.getElementsByClassName('spoiler');" + 
              "for(var i = 0; i < spoiler.length; ++i) {" + 
                  "spoiler[i].addEventListener('click', function() {" + 
                      "this.classList.toggle('revealed');" + 
                  "});" + 
              "}" + 
-         "})();" + 
+         "});" + 
          "</script>";
 
 hexo.extend.filter.register('after_render:html',function(str,data){
