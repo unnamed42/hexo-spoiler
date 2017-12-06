b0e4bd29e40df676606386ae38b08a06bb8dded4
unnamed42 <a390517426@gmail.com>
2017-12-06 19:03:26 +0800

    maybe fixed?

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const path    = require("path");
 const read    = require("hexo-fs").readFileSync;
-const cheerio = require('cheerio');
 
 const asset = `<style type="text/css">${read(path.join(__dirname, "src/spoiler.css"))}</style>` + 
               `<script type="text/javascript">${read(path.join(__dirname, "src/spoiler.js"))}</script>`;
@@ -16,12 +15,7 @@ hexo.extend.tag.register('spoiler', function(args) {
 });
 
 hexo.extend.filter.register('after_render:html', function(str, data) {
-    const $ = cheerio.load(str);
-    
-    if($(".spoiler").length) {
-        $("head").append(asset);
-        str = $.html();
-    }
-    
+    if(str.indexOf('span class="spoiler"') != -1) 
+        return str.replace(/<\/head>/i, `${asset}</head>`);
     return str;
 });
