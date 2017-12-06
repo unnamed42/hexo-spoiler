12b7ca250f63c88acbd6bc165aaa7ee67749a52c
unnamed42 <a390517426@gmail.com>
2017-12-06 14:49:24 +0800

    read file content rather than hard-coded string content

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
+"use strict";
+
+var path = require("path");
+var fs   = require("hexo-fs");
+
+var css = '<style type="text/css">' + fs.readFileSync(path.join(__dirname, "src/spoiler.css")) + "</style>";
+var js  = '<script type="text/javascript">' + fs.readFileSync(path.join(__dirname, "src/spoiler.js")) + "</script>";
+
 hexo.extend.tag.register('spoiler', function(args) {
-    
     var content = args.join(' ');
+    var html_code = hexo.render.renderSync({text: content, engine: "markdown"}).replace(/<\/?p>/g,'');
     
-    var html_code = hexo.render.renderSync({text: content, engine: "markdown"});
-    
-    html_code = html_code.replace(/<\/?p>/g,'');
-    
-    return "<span class=\"spoiler\">" + html_code + "</span>";
+    return '<span class="spoiler">' + html_code + "</span>";
 });
-
-var css = '<style type="text/css">' + 
-          "span.spoiler {" + 
-              "color: rgba(0, 0, 0, 0);" + 
-              "background-color: rgba(0, 0, 0, 0);" + 
-              "text-shadow: grey 0px 0px 10px;" +
-              "cursor: pointer;" +
-              "-webkit-transition: text-shadow .5s ease;" +
-              "-moz-transition: text-shadow .5s ease;" +
-              "transition: text-shadow .5s ease;" +
-          "}" +
-          "span.spoiler:hover {" +
-              "text-shadow: grey 0px 0px 5px;" +
-          "}" +
-          "span.spoiler.revealed {" +
-              "text-shadow: grey 0px 0px 0px;" +
-          "}" +
-          "</style>";
-
-var js = '<script type="text/javascript">' + 
-         "window.addEventListener('load', function(){" + 
-             "var spoiler = document.getElementsByClassName('spoiler');" + 
-             "for(var i = 0; i < spoiler.length; ++i) {" + 
-                 "spoiler[i].addEventListener('click', function() {" + 
-                     "this.classList.toggle('revealed');" + 
-                 "});" + 
-             "}" + 
-         "});" + 
-         "</script>";
 
 hexo.extend.filter.register('after_render:html',function(str,data){
     
-    if(str.indexOf("span class=\"spoiler\"") != -1)
+    if(str.indexOf('span class="spoiler"') != -1)
         str = str.replace(/<\s*\/\s*head\s*>/i, css + js + "</head>");
     
     return str;
