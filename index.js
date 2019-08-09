e097e46cf7681b123f9c969a4915108ce67c4f8d
unnamed42 <wenruih.cn@gmail.com>
2019-08-09 15:13:26 +0800

    bugfix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const asset = `
 window.addEventListener("load", function(){
     var spoiler = document.getElementsByClassName("spoiler");
     for(var i=0; i<spoiler.length; ++i){
-        spoiler.addEventListener("click", function(){
+        spoiler[i].addEventListener("click", function(){
             this.classList.toggle("revealed");
         });
     }
